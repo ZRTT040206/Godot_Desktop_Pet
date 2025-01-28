74021ff1e6bceaaa9992628b2a88db3fdf1db986
Jerry <2963025245@qq.com>
2025-01-28 15:03:18 +0800

    暂时失败，拖动实现有点复杂

--- tscn/Mymenu.cs
+++ tscn/Mymenu.cs
@@ -1,17 +1,19 @@
 using Godot;
 using System;
-
 public partial class Mymenu : Node2D
 {
+	private Vector2 pos;
+	private bool IsDragging = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		
 	}
 	
 	public override void _Input(InputEvent @event)
@@ -20,8 +22,29 @@ public partial class Mymenu : Node2D
 		//InputEventMouse类型的实例可以用ButtonMask检测输入的变量
 		if(mouse.IsPressed() && Input.IsActionPressed("click_left"))//Input.IsActionPressed("click_left")在键位映射中可以定义
 		{
-			GD.Print(mouse.ButtonMask);
+			pos = mouse.Position;
+			IsDragging = true;
+			pos = mouse.Position;
+			GD.Print(pos);
+			//GetTree().Root.Position+=pos;
 		}
+		else{
+			IsDragging = false;
+		}
+		if(IsDragging)
+		{
+			Vector2 poss = GetViewport().GetWindow().GetPosition() + (mouse.Position - pos);
+			GD.Print(poss);
+			Vector2I poss2 = new Vector2I((int)poss.X,(int)poss.Y);
+			GD.Print(poss2);
+			GetViewport().GetWindow().SetPosition(poss2);
+		}	
 		//GD.Print(mouse.Position);
+	}
+	public void closeAll()
+	{
+		GetTree().Quit();
+		// Node Roots= GetNode("/root");
+		// Roots.QueueFree();
 	}
 }
